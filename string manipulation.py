ee9b494c9d003294c23654b2f98d6ee2fce507b9
Muhammad Razin <mrazinanwar6@gmail.com>
2024-07-30 10:30:28 +0530

    Counted the number of vowels in the entered string and printed

--- string manipulation.py
+++ string manipulation.py
@@ -2,3 +2,15 @@ s = input('Enter string')
 print('Length of the string : '+str(len(s)))
 print('String in capital letters : '+s.upper())
 print('String in small letters : '+s.lower())
+count=0
+vowels='aeiouAEIOU'
+for char in s:
+    if char in vowels:
+        count+=1
+
+if count==0 :
+            print('No vowels found')
+
+else:
+            print('Vowels are : '+str(count))
+
